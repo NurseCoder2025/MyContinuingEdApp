ca2fc67b5894c189fa50d1e264fef6a767a54150
Darth Plageus <dplageus@sithtemple.net>
2025-07-08 19:00:58 -0400

    Initial Commit

---
+++ MyContinuingEdApp/MyContinuingEdAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyContinuingEdAppApp.swift
+//  MyContinuingEdApp
+//
+//  Created by Manann on 7/8/25.
+//
+
+import SwiftUI
+
+@main
+struct MyContinuingEdAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
