7aa9073848f033275c377bf346ef4b695451345d
Darth Nefarious <dnafarious@sith.net>
2025-08-19 21:33:34 -0400

    Added permission request for users' photo library; added ability to save a CE certificate image, PDF, or take a camera shot of one and save it to the selected activity.  Created helper functions for reading the binary file signature of a given certificate object.  Everything but the Apple HEIFF formats can be correctly identified.

---
+++ MyContinuingEdApp/CameraPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  CameraPickerView.swift
+//  MyContinuingEdApp
+//
+//  Created by Kamino on 8/19/25.
+//
+
+import SwiftUI
+
+struct CameraPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraPickerView()
+}
