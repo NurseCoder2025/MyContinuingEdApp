1f48e9499ce40d69d0f6f20273f889e783accf94
Michael Kieffer, RN <hipster.newt.2p@icloud.com>
2026-02-24 23:18:13 -0500

    Updated CertificateBrain with new logic that better handles UIDocument objects along with constants in String and URL to make it easier and less error-prone referencing filenames, specific URLs, and query result keys. Created a MediaFileVersion struct to serve as a wrapper object for NSFileVersion objects (so the Coordinator and object models can remain comformant to Codable) - has an internal method that can return a new NSFIleVersion object based on the struct's properties when needed.  Created two new protocols to help ensure all media-related coordinator and object models have the properties and protocol conformances needed.

---
+++ MyContinuingEdApp/Extensions/NSFileVersion+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  CKRecord+Extensions.swift
+//  MyContinuingEdApp
+//
+//  Created by Ilum on 2/21/26.
+//
+
+import CloudKit
+import Foundation
+
+
+
+
+
---
+++ MyContinuingEdApp/MediaFileVersion.swift
@@ -0,0 +1,60 @@
+//
+//  CertFileVersion.swift
+//  MyContinuingEdApp
+//
+//  Created by Ilum on 2/21/26.
+//
+
+import Foundation
+
+
+/// The purpose of the CertFileVersion struct is to allow for the holding of file version information and creation
+/// of NSFileVersion objects based on the URL in the struct's fileLocation property while conforming to Codable.
+///
+/// The main reason this struct was created was due to the fact that the NSFileVersion class conforms neither
+/// to Codable nor to NSCoding, so the only way to utilize that class in the CertificateCoordinator object, or
+/// anywhere else for that matter, was to essentially "wrap" it inside of a struct that could conform to Codable.
+struct MediaFileVersion: Codable, Equatable {
+    // MARK: - PROPERTIES
+    let fileLocation: URL
+    let version: Double
+    let localizedName: String?
+    let savedComputer: String?
+    let modifiedOn: Date?
+    
+    // MARK: - METHODS
+    
+    /// Struct method that allows for the creation of a NSFileVersion object based on
+    /// the fileLocation (URL) property for any instance.
+    /// - Returns: NSFileVersion instance if one can be made with the URL
+    /// provided.
+    func createFileVersionOfObject() -> NSFileVersion? {
+        NSFileVersion.currentVersionOfItem(at: fileLocation)
+    }//: createFileVersionObject(with)
+    
+    // MARK: - PROTOCOL CONFORMANCE
+    static func ==(lhs: MediaFileVersion, rhs: MediaFileVersion) -> Bool {
+        if lhs.fileLocation == rhs.fileLocation && lhs.version == rhs.version {
+            return true
+        } else {
+            return false
+        }
+    }//: ==
+    
+    // MARK: - INIT
+    init(fileAt: URL, version: Double) {
+        self.fileLocation = fileAt
+        self.version = version
+        
+        if let tempFV = NSFileVersion.currentVersionOfItem(at: fileAt) {
+            self.localizedName = tempFV.localizedName
+            self.savedComputer = tempFV.localizedNameOfSavingComputer
+            self.modifiedOn = tempFV.modificationDate
+        } else {
+            self.localizedName = nil
+            self.savedComputer = nil
+            self.modifiedOn = nil
+        }
+    }//: INIT
+    
+}//: STRUCT
